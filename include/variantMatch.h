97a10726a90bb583dc103b3c66d4c3c2ce335ea5
dvirtz <dvirtz@gmail.com>
2018-07-19 17:36:28 +0300

    support multi variant matching

--- include/variantMatch.h
+++ include/variantMatch.h
@@ -5,30 +5,21 @@
 
 namespace helpers {
 
-namespace detail {
-
-// a function object which calls one of the given overloads depending on the
-// variants value
-template <typename Variant> struct VariantMatcher {
-  VariantMatcher(Variant &variant) : m_variant(variant) {}
-
-  template <typename... Funcs> auto operator()(Funcs &&... fs) const {
-    auto visitor = overload(std::forward<Funcs>(fs)...);
-    return boost::apply_visitor(visitor, m_variant);
-  }
-
-  Variant &m_variant;
-};
-
-} // namespace detail
-
-template <typename Variant> auto match(Variant &&variant) {
-  return detail::VariantMatcher<Variant>{std::forward<Variant>(variant)};
+template <typename... Variants> auto match(Variants &&... variants) {
+  return [&](auto &&... funcs) {
+    auto visitor = overload(std::forward<decltype(funcs)>(funcs)...);
+    return boost::apply_visitor(visitor, std::forward<Variants>(variants)...);
+  };
 }
 
-template <typename Variant, typename... Types> void assertTypes(const Variant &variant) {
+template <typename T, typename Variant> bool hasType(const Variant &variant) {
+  return boost::get<T>(&variant) != nullptr;
+}
+
+template <typename Variant, typename... Types>
+void assertTypes(const Variant &variant) {
   bool result = false;
-  (void)std::initializer_list<int>{(result |= (boost::get<Types>(&variant) != nullptr), 0)...};
+  (void)std::initializer_list<int>{(result |= hasType<Types>(variant), 0)...};
   assert(result && "variant should have one of the listed types");
 }
 
